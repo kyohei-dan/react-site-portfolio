6fc03bd43783e27641fbf2728de4e66d261bc049
kyohei_dan <kyohey.dan@gmail.com>
2021-08-22 20:53:10 +0900

    コードの修正

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,3 @@
-//This is home page, It will contains all the sections require in this page.
-
-//Import all the require sections here
 import HeroSection from "../Sections/Hero/index";
 import About from "../Sections/About/index";
 import Services from "../Sections/Services/index";
@@ -9,23 +6,23 @@ import Contact from "../Sections/Contact/index";
 import styled from "styled-components";
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  /* position: relative; */
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	/* position: relative; */
 `;
 
 const Home = () => {
-  return (
-    <Container>
-      <HeroSection />
-      <About />
-      <Services />
-      <Testimonials />
-      <Contact />
-    </Container>
-  );
+	return (
+		<Container>
+			<HeroSection />
+			<About />
+			<Services />
+			<Testimonials />
+			<Contact />
+		</Container>
+	);
 };
 
 export default Home;
--- src/Sections/About/index.js
+++ src/Sections/About/index.js
@@ -11,149 +11,147 @@ const move = keyframes`
 `;
 
 const AboutSection = styled.section`
-  width: 100vw;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+	width: 100vw;
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 `;
 const Waves = styled.img`
-  width: 100%;
-  height: auto;
-  position: absolute;
-  top: -1rem;
+	width: 100%;
+	height: auto;
+	position: absolute;
+	top: -1rem;
 `;
 const Hand = styled.div`
-  position: absolute;
-  bottom: -1rem;
-  right: 0;
+	position: absolute;
+	bottom: -1rem;
+	right: 0;
 
-  @media only Screen and (max-width: 40em) {
-    display: none;
-  }
+	@media only Screen and (max-width: 40em) {
+		display: none;
+	}
 `;
 
 const Main = styled.div`
-  margin: 0 15rem;
-  margin-top: 15rem;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  @media only Screen and (max-width: 64em) {
-    margin: 0 calc(5rem + 5vw);
-    margin-top: 10rem;
-  }
-  @media only Screen and (max-width: 40em) {
-    align-items: center;
-    margin: 3rem calc(3rem + 3vw);
-  }
+	margin: 0 15rem;
+	margin-top: 15rem;
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
+	@media only Screen and (max-width: 64em) {
+		margin: 0 calc(5rem + 5vw);
+		margin-top: 10rem;
+	}
+	@media only Screen and (max-width: 40em) {
+		align-items: center;
+		margin: 3rem calc(3rem + 3vw);
+	}
 `;
 
 const Title = styled.h1`
-  font-size: 2rem;
-  display: inline-block;
+	font-size: 2rem;
+	display: inline-block;
 `;
 
 const CurvedLine = styled.div`
-  width: 7rem;
-  height: 2rem;
-  border: solid 5px var(--purple);
-  border-color: var(--purple) transparent transparent transparent;
-  border-radius: 150%/60px 70px 0 0;
+	width: 7rem;
+	height: 2rem;
+	border: solid 5px var(--purple);
+	border-color: var(--purple) transparent transparent transparent;
+	border-radius: 150%/60px 70px 0 0;
 `;
 
 const Content = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  @media only Screen and (max-width: 40em) {
-    flex-direction: column;
-  }
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	@media only Screen and (max-width: 40em) {
+		flex-direction: column;
+	}
 `;
 
 const Rocket = styled.div`
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  width: 40%;
-  padding-bottom: 5rem;
-  animation: ${move} 2.5s ease infinite;
-  @media only Screen and (max-width: 40em) {
-    width: 50vw;
-    padding-bottom: 0;
-  }
+	display: flex;
+	justify-content: center;
+	align-content: center;
+	width: 40%;
+	padding-bottom: 5rem;
+	animation: ${move} 2.5s ease infinite;
+	@media only Screen and (max-width: 40em) {
+		width: 50vw;
+		padding-bottom: 0;
+	}
 `;
 
 const Human = styled.div`
-  width: 50%;
-  position: absolute;
-  right: 0;
-  bottom: 100%;
+	width: 50%;
+	position: absolute;
+	right: 0;
+	bottom: 100%;
 
-  @media only Screen and (max-width: 40em) {
-    display: none;
-  }
+	@media only Screen and (max-width: 40em) {
+		display: none;
+	}
 `;
 const Text = styled.h4`
-  font-size: calc(0.5rem + 1vw);
-  line-height: 1.5;
-  color: var(--nav2);
+	font-size: calc(0.5rem + 1vw);
+	line-height: 1.5;
+	color: var(--nav2);
 `;
 const Circle = styled.span`
-  display: inline-block;
-  width: 1rem;
-  height: 1rem;
-  border-radius: 50%;
-  background-color: black;
-  margin-right: 0.5rem;
-  margin-top: 1rem;
+	display: inline-block;
+	width: 1rem;
+	height: 1rem;
+	border-radius: 50%;
+	background-color: black;
+	margin-right: 0.5rem;
+	margin-top: 1rem;
 `;
 const AboutText = styled.div`
-  width: 50%;
-  position: relative;
-  @media only Screen and (max-width: 40em) {
-    width: 100%;
-  }
+	width: 50%;
+	position: relative;
+	@media only Screen and (max-width: 40em) {
+		width: 100%;
+	}
 `;
 
 const About = () => {
-  return (
-    <AboutSection id="about">
-      <Waves src={wave} alt="" />
-      <Hand>
-        <img src={hand} alt="" />
-      </Hand>
-      <Main>
-        <div>
-          <Title>About Us</Title>
-          <CurvedLine />
-        </div>
-        <Content>
-          <Rocket>
-            <img src={rocket} alt="" width="400" height="400" />
-          </Rocket>
-          <AboutText>
-            <Human>
-              <img src={human} alt="" width="400" height="400" />
-            </Human>
+	return (
+		<AboutSection id="about">
+			<Waves src={wave} alt="" />
+			<Hand>
+				<img src={hand} alt="" />
+			</Hand>
+			<Main>
+				<div>
+					<Title>About Us</Title>
+					<CurvedLine />
+				</div>
+				<Content>
+					<Rocket>
+						<img src={rocket} alt="" width="400" height="400" />
+					</Rocket>
+					<AboutText>
+						<Human>
+							<img src={human} alt="" width="400" height="400" />
+						</Human>
 
-            <Text>
-              We help our customers to tell about themselves, to grow and stand
-              out in an increasingly competitive digital world, through creative
-              projects that are able to attract and involve, creating strategic
-              value.
-            </Text>
-            <div>
-              <Circle style={{ backgroundColor: "var(--purple)" }} />
-              <Circle style={{ backgroundColor: "var(--pink)" }} />
-              <Circle style={{ backgroundColor: "var(--black)" }} />
-            </div>
-          </AboutText>
-        </Content>
-      </Main>
-    </AboutSection>
-  );
+						<Text>
+							ウェブの各種エキスパートが連携したチーム体制が私たちの強み。
+							戦略立案からクリエイティブ、アフターフォローまでシームレスに対応いたします。
+						</Text>
+						<div>
+							<Circle style={{ backgroundColor: "var(--purple)" }} />
+							<Circle style={{ backgroundColor: "var(--pink)" }} />
+							<Circle style={{ backgroundColor: "var(--black)" }} />
+						</div>
+					</AboutText>
+				</Content>
+			</Main>
+		</AboutSection>
+	);
 };
 
 export default About;
